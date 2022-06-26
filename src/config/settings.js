49ac5be9487deb248c69cac1f8cfbdba23734bad
darulhidayah <108205971+darulhidayah@users.noreply.github.com>
2022-06-26 14:39:24 +0900

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyBkIOH3--o1_5skFfK03EOr8uKO8-WcOJc',
+  authDomain: 'mdh2-e895e.firebaseapp.com',
+  databaseURL: 'https://mdh2-e895e-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'mdh2-e895e',
+  storageBucket: 'mdh2-e895e.appspot.com',
+  messagingSenderId: '587311239291',
+  appId: '1:587311239291:web:a335fa82f444a6e33e12de'
 }
 
 export { firebaseConfig }
